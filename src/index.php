4394f61c0677efb325f4a545c997ba0140fe7db8
Alessandro Massarenti <48263116+alessandro-massarenti@users.noreply.github.com>
2021-11-04 16:08:28 +0100

    Aggiunta la struttura base delle pagine da aggiungere

---
+++ src/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>@-Track - Elba</title>
+
+</head>
+<body>
+    <h1>@-Track - Elba</h1>
+</body>
+</html>
---
+++ src/segnalazioni.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>@-Track - Elba</title>
+
+</head>
+<body>
+    <h1>Segnalazioni - @-Track - Elba</h1>
+    <p>Vedi tutte le segnalazioni della zona</p>
+</body>
+</html>
