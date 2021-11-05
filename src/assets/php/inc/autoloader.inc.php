afa9dfcd510340a872375e402b4af76bf4c104ba
Alessandro Massarenti <48263116+alessandro-massarenti@users.noreply.github.com>
2021-11-05 09:56:38 +0100

    Aggiunto il file che autocarica le classi quando è necessaria una classe.

---
+++ src/assets/php/inc/autoloader.inc.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+Per attivare l'autoloading delle classi specifiche richieste aggiungere la seguente linea di codice al file che ha bisogno di questa funzionalità
+
+    include 'includes/autoloader.inc.php';
+*/
+
+spl_autoload_register('myAutoLoader');
+
+function myAutoLoader($className){
+    $path = "classes/";
+    $extension = ".class.php";
+
+    $fullPath = $path . $className . $extension;
+
+    if(!file_exists($fullPath)){
+        return false;
+    }
+
+    include_once $fullPath;
+}
