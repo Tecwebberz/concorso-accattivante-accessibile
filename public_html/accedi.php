112d3a8cb2f9ab07527e41510a51007411a47f6f
alecsferra <alecsferra@protonmail.com>
2022-01-10 18:03:52 +0100

    Compratevi un vostro server barbonni

--- public_html/accedi.php
+++ public_html/accedi.php
@@ -3,12 +3,12 @@ $root = ".";
 $page_index = 4;
 require_once($root . "/app/global.php");
 
-$login_template = $template_engine->load_template("login.template.html");
+$login_template = $template_engine->load_template("accedi.template.html");
 $login_template->insert("header", build_header());
 
 $error = "";
 if (isset($_GET["error"]) && $_GET["error"] == UserServiceError::AUTH_FAILED) {
-    $error = "Errore di accesso";
+    $error = make_error("Credenziali errate!");
 }
 
 $login_template->insert("maybe_error", $error);
