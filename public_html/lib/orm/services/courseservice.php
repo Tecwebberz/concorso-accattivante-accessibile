ce300151ae0dbba9ecd69238bc57c3cb47d6ae46
Elia Pasquali <45105755+eliapasquali@users.noreply.github.com>
2022-01-14 17:45:39 +0100

    modifica corsi

--- public_html/lib/orm/services/courseservice.php
+++ public_html/lib/orm/services/courseservice.php
@@ -11,22 +11,28 @@ class CourseService {
 
     public function __construct(DatabaseLayer $db) {
         $this->db = $db;
-        $this->table_name = "Corso";
+        $this->table_name = "Course";
         $this->table_name_userrel = "Iscrizioni_corsi";
     }
 
     public function replace(CourseDTO $course) {
         $res = $this->db->query(
             "REPLACE INTO {$this->table_name}
-                (id_corso, nome, email_responsabile,
-                 attivo, anno_attivazione)
+                ('id_corso', 'name', 'description', 'cfu',
+                 'propedeutici', 'anno', 'periodo', 'lingua',
+                 'responsabile', 'email_resp') 
              VALUES (?, ?, ?, ?, ?)",
             array(
-                array("i", $course->id),
+                array("i", $course->id_corso),
                 array("s", $course->name),
-                array("s", $course->emal_prof),
-                array("i", $course->active),
-                array("s", $course->activation_year),
+                array("s", $course->description),
+                array("i", $course->cfu),
+                array("s", $course->propedeutici),
+                array("i", $course->anno),
+                array("i", $course->periodo),
+                array("s", $course->lingua),
+                array("s", $course->responsabile),
+                array("s", $course->email_resp)
             )
         );
         return CourseServiceError::OK;
