a062cdc2314220a34d09a38138d6f4b1d1a269ff
Elia Pasquali <45105755+eliapasquali@users.noreply.github.com>
2022-01-14 21:23:09 +0100

    template corsi

--- public_html/lib/orm/data/coursedto.php
+++ public_html/lib/orm/data/coursedto.php
@@ -3,18 +3,28 @@
 class CourseDTO {
     public int $id;
     public string $name;
-    public string $emal_prof;
-    public int $active;
-    public string $activation_year;
+    public string $description;
+    public int $cfu;
+    public string $propedeutici;
+    public int $anno;
+    public int $periodo;
+    public string $lingua;
+    public string $responsabile;
+    public string $email_resp;
 }
 
 function parse_course(array $row) : CourseDTO {
     $course = new CourseDTO();
-    $course->id = $row["id_corso"];
-    $course->name = $row["nome"];
-    $course->emal_prof = $row["email_responsabile"];
-    $course->active = $row["attivo"];
-    $course->activation_year = $row["anno_attivazione"];
+    $course->id_corso = $row["id_corso"];
+    $course->name = $row["name"];
+    $course->description = $row["description"];
+    $course->cfu = $row["cfu"];
+    $course->propedeutici = $row["propedeutici"];
+    $course->anno = $row["anno"];
+    $course->periodo = $row["periodo"];
+    $course->lingua = $row["lingua"];
+    $course->responsabile = $row["responsabile"];
+    $course->email_resp = $row["email_resp"];
     return $course;
 }
 ?>
