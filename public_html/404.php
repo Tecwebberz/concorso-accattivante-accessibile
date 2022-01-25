f049b536cefcc070e3f4b66e7fb3703dd274615a
alecsferra <alecsferra@protonmail.com>
2022-01-25 16:34:35 +0100

    404

--- public_html/404.php
+++ public_html/404.php
@@ -6,9 +6,24 @@ require_once($root . "/app/global.php");
 $page_template = $template_engine->load_template("404.template.html");
 $page_template->insert("header", build_header());
 
+function build_base(): string {
+    $srv = "http://{$_SERVER['SERVER_NAME']}:{$_SERVER['SERVER_PORT']}";
+
+    $ruri = $_SERVER['REQUEST_URI'];
+    $exp = explode("/", $ruri, 3);
+
+    $uri = "";
+    if (count($exp) === 3) {
+        $uri = "/{$exp[1]}";
+    }
+
+    return "{$srv}{$uri}/";
+}
+
 if (isset($_GET["error"]) && $_GET["error"] === "corso") {
     $page_template->insert_all(array(
         "from" => "stavi cercando un corso?",
+        "base" => build_base(),
         "to"   => "Vedi tutti i " . make_link("corsi", "corsi.php") .
                   " oppure torna alla " . make_link("pagina principale", "index.php") . ".",
     ));
@@ -21,6 +36,7 @@ if (isset($_GET["error"]) && $_GET["error"] === "corso") {
 } else {
     $page_template->insert_all(array(
         "from" => "puoi tornare alla",
+        "base" => build_base(),
         "to"   => make_link("pagina principale", "index.php") . ".",
     ));
 }
