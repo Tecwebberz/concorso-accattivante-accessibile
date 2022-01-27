6bcf580caf67c86cbd7d2b92fc7f4d3a55fefb04
alecsferra <alecsferra@protonmail.com>
2022-01-27 21:56:13 +0100

    sort aule on button

--- public_html/assets/script/sort_aule.js
+++ public_html/assets/script/sort_aule.js
@@ -12,41 +12,43 @@ const computeDistance = (pos1, pos2) => {
     return earthRadiusKm * norm;
 };
 
-// Triggerato iff da il permesso ed è supportato
-navigator.geolocation.getCurrentPosition(pos => {
-    const aule = document.querySelectorAll(".cards .card");
+const sort = () => {
+    // Triggerato iff da il permesso ed è supportato
+    navigator.geolocation.getCurrentPosition(pos => {
+        const aule = document.querySelectorAll(".cards .card");
 
-    // Aggiungi informazio sulla distanza
-    aule.forEach(aula => {
-        const distance = computeDistance({
-            lat: parseFloat(aula.dataset.lat),
-            lon: parseFloat(aula.dataset.lon)
-        }, {
-            lat: pos.coords.latitude,
-            lon: pos.coords.longitude
-        }, );
-        aula.dataset.distance = distance;
+        // Aggiungi informazio sulla distanza
+        aule.forEach(aula => {
+            const distance = computeDistance({
+                lat: parseFloat(aula.dataset.lat),
+                lon: parseFloat(aula.dataset.lon)
+            }, {
+                lat: pos.coords.latitude,
+                lon: pos.coords.longitude
+            }, );
+            aula.dataset.distance = distance;
 
-        const extraSection = aula.querySelector(".extra");
-        const element = document.createElement("p");
-        element.classList.add("distance");
-        element.textContent = `Distanza: ${distance.toFixed(2)} km`;
-        extraSection.prepend(element);
+            const extraSection = aula.querySelector(".extra");
+            const element = document.createElement("p");
+            element.classList.add("distance");
+            element.textContent = `Distanza: ${distance.toFixed(2)} km`;
+            extraSection.append(element);
+        });
+
+        // Ordina il carte in base a distanza e apertura
+        const compare = (e1, e2) => {
+            if (e1.dataset.open == e2.dataset.open)
+                return e1.dataset.distance - e2.dataset.distance
+            return e1.dataset.open - e2.dataset.open
+        }
+
+        const auleClean = Array.prototype.slice.call(aule, 0);
+        auleClean.sort(compare);
+
+        // Rimpiazza la lista vecchia
+        const oldRoot = document.querySelector(".cards");
+        const root = oldRoot.cloneNode(false);
+        auleClean.forEach(it => root.appendChild(it));
+        oldRoot.parentNode.replaceChild(root, oldRoot);
     });
-
-    // Ordina il carte in base a distanza e apertura
-    const compare = (e1, e2) => {
-        if (e1.dataset.open == e2.dataset.open)
-            return e1.dataset.distance - e2.dataset.distance
-        return e1.dataset.open - e2.dataset.open
-    }
-
-    const auleClean = Array.prototype.slice.call(aule, 0);
-    auleClean.sort(compare);
-
-    // Rimpiazza la lista vecchia
-    const oldRoot = document.querySelector(".cards");
-    const root = oldRoot.cloneNode(false);
-    auleClean.forEach(it => root.appendChild(it));
-    oldRoot.parentNode.replaceChild(root, oldRoot);
-});
+}
