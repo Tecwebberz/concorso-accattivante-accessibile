fbc5ca78846219b85b3d85ade0c0cbdcf6d79d2a
Elia Pasquali <45105755+eliapasquali@users.noreply.github.com>
2022-01-28 15:24:24 +0100

    div to section

--- public_html/assets/script/sort_aule.js
+++ public_html/assets/script/sort_aule.js
@@ -1,22 +1,14 @@
-if ("geolocation" in navigator) {
-    const section = document.getElementsByClassName("centered")[0];
-    const button = document.createElement("button");
-    button.classList.add("btn", "btn-primary");
-    button.onclick = "sort()";
-    button.innerText = "Ordina in base alla distanza";
-    section.insertBefore(button, section.children[1]);
-}
 
 const deg2rad = (deg) => deg * Math.PI / 180;
 const computeDistance = (pos1, pos2) => {
     const earthRadiusKm = 6371;
     const deltaLat = deg2rad(pos2.lat - pos1.lat);
     const deltaLon = deg2rad(pos2.lon - pos1.lon);
-
+    
     const alpha =
-        Math.pow(Math.sin(deltaLat / 2), 2) +
-        Math.cos(deg2rad(pos1.lat)) * Math.cos(deg2rad(pos2.lat)) *
-        Math.pow(Math.sin(deltaLon / 2), 2);
+    Math.pow(Math.sin(deltaLat / 2), 2) +
+    Math.cos(deg2rad(pos1.lat)) * Math.cos(deg2rad(pos2.lat)) *
+    Math.pow(Math.sin(deltaLon / 2), 2);
     const norm = 2 * Math.atan2(Math.sqrt(alpha), Math.sqrt(1 - alpha));
     return earthRadiusKm * norm;
 };
@@ -25,7 +17,7 @@ const sort = () => {
     // Triggerato iff da il permesso ed è supportato
     navigator.geolocation.getCurrentPosition(pos => {
         const aule = document.querySelectorAll(".cards .card");
-
+        
         // Aggiungi informazio sulla distanza
         aule.forEach(aula => {
             const distance = computeDistance({
@@ -43,21 +35,31 @@ const sort = () => {
             element.textContent = `Distanza: ${distance.toFixed(2)} km`;
             extraSection.append(element);
         });
-
+        
         // Ordina il carte in base a distanza e apertura
         const compare = (e1, e2) => {
             if (e1.dataset.open == e2.dataset.open)
-                return e1.dataset.distance - e2.dataset.distance
+            return e1.dataset.distance - e2.dataset.distance
             return e1.dataset.open - e2.dataset.open
         }
-
+        
         const auleClean = Array.prototype.slice.call(aule, 0);
         auleClean.sort(compare);
-
+        
         // Rimpiazza la lista vecchia
         const oldRoot = document.querySelector(".cards");
         const root = oldRoot.cloneNode(false);
         auleClean.forEach(it => root.appendChild(it));
         oldRoot.parentNode.replaceChild(root, oldRoot);
     });
+}
+
+if ("geolocation" in navigator) {
+    const section = document.getElementsByClassName("centered")[0];
+    const button = document.createElement("button");
+    button.id = "order";
+    button.classList.add("btn", "btn-primary");
+    button.addEventListener('click', sort);
+    button.innerText = "Ordina in base alla distanza";
+    section.insertBefore(button, section.children[1]);
 }
