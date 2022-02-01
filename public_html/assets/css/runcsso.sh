8414a8d4ffe65d7350075a85cebe8cc6ec60aff0
Elia Pasquali <45105755+eliapasquali@users.noreply.github.com>
2022-02-01 15:27:19 +0100

    risolto inherit rotto in chrome

--- public_html/assets/css/runcsso.sh
+++ public_html/assets/css/runcsso.sh
@@ -3,3 +3,4 @@
 for file in $(find . -regex "./[a-zA-Z_-]*.css"); do
   csso $file --no-restructure -o "$(basename $file .css).min.css"
 done
+  echo "Fogli di stile minimizzati!"
--- public_html/assets/script/runterser.sh
+++ public_html/assets/script/runterser.sh
@@ -3,3 +3,4 @@
 for file in $(find . -regex "./[a-zA-Z_-]*.js"); do
   terser --compress --mangle -- $file > "$(basename $file .js).min.js"
 done
+echo "Script mimizzati!"
