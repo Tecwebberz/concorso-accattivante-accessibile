724f0511a767ee472abfdfc4e30901c9c67e212d
Elia Pasquali <45105755+eliapasquali@users.noreply.github.com>
2022-05-04 21:24:06 +0200

    rename trasporti to mobilità

--- public_html/mobilita.php
+++ public_html/mobilita.php
@@ -2,7 +2,7 @@
 $root = ".";
 require_once($root . "/app/global.php");
 
-$trasporti_template = $template_engine->load_template("trasporti.template.html");
+$trasporti_template = $template_engine->load_template("mobilita.template.html");
 $trasporti_template->insert("header", build_header());
 $trasporti_template->insert("footer", build_footer());
 
