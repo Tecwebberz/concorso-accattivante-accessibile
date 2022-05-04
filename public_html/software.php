34395f4b3e3a7f42256fe4d6bdd74a0966b43a3e
Alessandro Massarenti <48263116+alessandro-massarenti@users.noreply.github.com>
2022-05-04 21:20:58 +0200

    Aggiunta la pagina relativa ai software Close #4

--- public_html/software.php
+++ public_html/software.php
@@ -3,11 +3,11 @@
 $root = ".";
 require_once($root . "/app/global.php");
 
-$servizi_template = $template_engine->load_template("software.template.html");
+$software_template = $template_engine->load_template("software.template.html");
 
-$servizi_template ->insert("header", build_header());
-$servizi_template->insert("footer", build_footer());
+$software_template ->insert("header", build_header());
+$software_template->insert("footer", build_footer());
 
-echo $servizi_template->build();
+echo $software_template->build();
 
 ?>
